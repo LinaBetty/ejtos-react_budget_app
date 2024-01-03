4b7eb393a1d2b75e112c78765083ce7ba3f0de73
LinaB <lina.uribazo@outlook.com>
2024-01-02 22:21:23 -0500

    nuevos cambios

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -42,8 +42,8 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button class="btn btn-success" onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><button class="btn btn-danger" onClick={event=> decAllocation(props.name)}>-</button></td>
+        <td><button style={{fontSize:"150%",padding: "0px 6px",borderRadius:"50%", fontWeight:"bold"}} class="btn btn-success" onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button style={{fontSize:"150%",padding: "0px 9px",borderRadius:"50%", fontWeight:"bold"}} class="btn btn-danger" onClick={event=> decAllocation(props.name)}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
